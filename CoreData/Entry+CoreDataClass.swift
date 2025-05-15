44f02a2aea37b910c7d6a8883f3f5e0c41414298
n-ndo <fa.malcaluque@gmail.com>
2025-05-15 05:46:51 -0700

    Removed unneccessary tracking + .gitignore

---
+++ CoreData/Entry+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  Entry+CoreDataClass.swift
+//  csc680-final-project
+//
+//  By Fernando Abel Malca Luque
+
+
+import Foundation
+import CoreData
+
+
+public class Entry: NSManagedObject {
+
+}
